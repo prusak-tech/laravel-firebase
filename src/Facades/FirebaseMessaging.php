6e99f57d73d6e0bbb77703b6a6d9ee0886ef5db6
Michał Prusak <michal.prusak@itsailors.tech>
2019-12-27 12:08:54 +0100

    Cumulative update for the Lumen framework

--- src/Facades/FirebaseMessaging.php
+++ src/Facades/FirebaseMessaging.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Kreait\Laravel\Firebase\Facades;
+namespace PrusakTech\Laravel\Firebase\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Kreait\Laravel\Firebase;
+namespace PrusakTech\Laravel\Firebase;
 
 use Illuminate\Contracts\Foundation\Application;
 use Kreait\Firebase;
